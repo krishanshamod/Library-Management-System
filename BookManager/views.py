b812f9ddcdd0acf932bab2cdd21541ed95c9511d
Krishan Shamod <krishanshamod@gmail.com>
2022-05-11 11:23:05 +0530

    Added add books functionality

--- BookManager/views.py
+++ BookManager/views.py
@@ -1,5 +1,18 @@
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from django.http.response import JsonResponse
+
+from BookManager.models import Book
+from BookManager.serializers import BookSerializer
 
 
+@csrf_exempt
 def add_book(request):
-    return HttpResponse("Added the book")
+    book_data = JSONParser().parse(request)
+    book_serializer = BookSerializer(data=book_data)
+    if book_serializer.is_valid():
+        book_serializer.save()
+        return JsonResponse("Added Successfully", safe=False)
+    return JsonResponse("Failed to add", safe=False)
+
